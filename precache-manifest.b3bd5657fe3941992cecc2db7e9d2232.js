2250adf6f7cfcc3150704f7fe9cf8b95488ce0d6
Afteni Daniel <afteni.danu@gmail.com>
2020-09-14 13:05:03 +0300

    Updates

---
+++ precache-manifest.b3bd5657fe3941992cecc2db7e9d2232.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7de3ca0c71a17f3f29c69e70c7970fb6",
+    "url": "/coin-exchange/index.html"
+  },
+  {
+    "revision": "1ea122c1fcd605ae273f",
+    "url": "/coin-exchange/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "88052c20d4e201c67925",
+    "url": "/coin-exchange/static/js/2.041970dd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/coin-exchange/static/js/2.041970dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ea122c1fcd605ae273f",
+    "url": "/coin-exchange/static/js/main.ffa1bc66.chunk.js"
+  },
+  {
+    "revision": "93828d2b106e12e6dd69",
+    "url": "/coin-exchange/static/js/runtime-main.1ad36b6f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/coin-exchange/static/media/logo.5d5d9eef.svg"
+  }
+]);
